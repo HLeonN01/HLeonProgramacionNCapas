e765e1ab0498fe46fa37b7caceea3231d6df5904
HLeonN01 <hugoln01@outlook.com>
2025-05-13 18:51:00 -0600

    Mapas agregados a la vista con crud terminado

--- PL_MVC/Controllers/EmpresaController.cs
+++ PL_MVC/Controllers/EmpresaController.cs
@@ -8,13 +8,46 @@ namespace PL_MVC.Controllers
 {
     public class EmpresaController : Controller
     {
-        // GET: Empresa
+        [HttpGet]
         public ActionResult GetAll()
         {
             ML.Result result = BL.Empresa.GetAll();
             ML.Empresa empresa = new ML.Empresa();
             empresa.Empresas = result.Objects;
             return View(empresa);
+        }
+
+        [HttpGet]
+        public ActionResult Form(int? IdEmpresa)
+        {
+            ML.Empresa empresa = new ML.Empresa();
+            if (IdEmpresa > 0)
+            {
+                ML.Result update = BL.Empresa.GetById(IdEmpresa.Value);
+                empresa = (ML.Empresa)update.Object;
+            }
+            return View(empresa);
+        }
+
+        [HttpPost]
+        public ActionResult Form(ML.Empresa empresa)
+        {
+            if (empresa.IdEmpresa == 0)
+            {
+                ML.Result add = BL.Empresa.Add(empresa);
+            }
+            else
+            {
+                ML.Result update = BL.Empresa.Update(empresa);
+            }
+            return RedirectToAction("GetAll");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int IdEmpresa)
+        {
+            ML.Result delete = BL.Empresa.Delete(IdEmpresa);
+            return RedirectToAction("GetAll");
         }
     }
 }
